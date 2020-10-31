ea5e84eef1f33475a9d6990c5c9aa61de2d65456
hanye <hanye@outlook.com>
2020-10-31 23:32:36 +0800

    每天coding一小时

--- eureka/eureka-server/src/main/java/com/hy/EurekaServerApplication.java
+++ eureka/eureka-server/src/main/java/com/hy/EurekaServerApplication.java
@@ -13,8 +13,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EurekaServerApplication.class)
-                .web(WebApplicationType.SERVLET)
-                .run(args);
+        new SpringApplicationBuilder(EurekaServerApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
